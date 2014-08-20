ef8dfe8db9c222916d58df5c4739cdcc38644334
wisup <f.e.a.rcombat@hotmail.fr>
2014-08-20 02:48:39 -0400

    Create index.php

---
+++ index.php
@@ -0,0 +1,32 @@
+<html>
+	<head>
+		<link href="taquin.css" type="text/css" rel="stylesheet"/>
+		<script src="taquin.js"></script>
+		<script type='text/javascript'>
+			/*function load(){
+			game.level = "easy";
+			game.init();
+			}*/
+		</script>
+	</head>
+	
+	<body>
+		<div id="window">
+			<h1>Taquin</h1>
+			
+			<div id="NewGame">
+				<h2>New Game:</h2>
+				<p>Choose difficulty:</p>
+				<div id="chooseDifficulty">
+					<div id="easy" class="button"><div class="op"></div>EASY</div>
+					<div id="medium" class="button"><div class="op"></div>MEDIUM</div>
+					<div id="hard" class="button"><div class="op"></div>HARD</div>
+				</div>
+				<div id="start" class="button">Start</div>
+			</div>
+			
+			<div id="game">
+			</div>
+		</div>
+	</body>
+</html>
